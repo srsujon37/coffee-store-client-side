d82452e52fc199a281406604bdb8ec4058155ebf
srsujon37 <srsujon97@gmail.com>
2025-05-26 01:54:14 +0600

    deploy

---
+++ src/components/Users2.jsx
@@ -0,0 +1,124 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Users2 = () => {
+
+    // const [users, setUsers] = useState([]);
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/user')
+    //         .then(res => res.json())
+    //         .then(data => setUsers(data))
+    // },[])
+
+    const {isPending, isError, error, data: users} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () =>{
+            const res = await fetch('http://localhost:3000/users');
+            return res.json()
+        }
+    })
+    if(isPending){
+        return <span className='loading loading-ring text-6xl'></span>
+    }
+    if(isError){
+        return <p>{error.massage}</p>
+    }
+    const handleDelete = (id) =>{
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        fetch(`http://localhost:3000/users/${id}`, {
+                            method: "DELETE",
+                        })
+                        .then(res => res.json())
+                        .then(data =>{
+                            console.log('after delete', data);
+                            if (data.deletedCount) {
+                                // const remainingUsers = users.filter(user => user._id !== id);
+                                //  setUsers(remainingUsers)
+                                // // TODO Delete user from firebase
+                                // Swal.fire({
+                                //     title: "Deleted!",
+                                //     text: "Your file has been deleted.",
+                                //     icon: "success"
+                                // });
+                            }
+                        })
+                    }
+                });
+        }
+    return (
+        <div>
+            {/* <h1 className='text-3xl'>Users: {users.length}</h1> */}
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>
+                        <label>
+                            No:
+                        </label>
+                        </th>
+                        <th>Name</th>
+                        <th>Job</th>
+                        <th>Email</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/* row 1 */}
+
+                        {
+                            users?.map((user, index) => <tr key={user._id}>
+                        <th>
+                        <label>
+                            {index + 1}
+                        </label>
+                        </th>
+                        <td>
+                        <div className="flex items-center gap-3">
+                            <div className="avatar">
+                            <div className="mask mask-squircle h-12 w-12">
+                                <img
+                                src={user.photo}
+                                alt="Avatar Tailwind CSS Component" />
+                            </div>
+                            </div>
+                            <div>
+                            <div className="font-bold">{user.name}</div>
+                            <div className="text-sm opacity-50">{user.address}</div>
+                            </div>
+                        </div>
+                        </td>
+                        <td>
+                        {user.phone}
+                        <br />
+                        <span className="badge badge-ghost badge-sm"></span>
+                        </td>
+                        <td>{user.email}</td>
+                        <th>
+                        <button className="btn btn-xs">V</button>
+                        <button className="btn btn-xs">E</button>
+                        <button onClick={() =>handleDelete(user._id)} className="btn btn-xs">X</button>
+                        </th>
+                    </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Users2;
--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,13 @@ import SignUp from './components/SignUp.jsx';
 import AuthProvider from './context/AuthProvider.jsx';
 import Users from './components/Users.jsx';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import Users2 from './components/Users2.jsx';
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
@@ -52,6 +59,10 @@ const router = createBrowserRouter([
         path: 'users',
         loader: () => fetch('http://localhost:3000/users'),
         Component: Users
+      },
+      {
+        path: 'users2',
+        Component: Users2
       }
     ]
   },
@@ -60,8 +71,12 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <RouterProvider router={router} />
     </AuthProvider>
+    </QueryClientProvider>
+
   </StrictMode>,
 )
